a30f27402028ea13639f2d28febc5f035716e45a
Anousack(아누삭) <127280201+AKXYK7533@users.noreply.github.com>
2024-01-24 17:27:15 +0900

    Delete hello_world/hello_world directory

--- hello_world/hello_world/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	printf("Hello");
-
-	int c = 1 + 1;
-	return 0;
-	//here something that I need to do with C programming
-}
--- hello_world/hello_world/test1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	printf("Senior is the Best!");
-
-	return 0;
-}
